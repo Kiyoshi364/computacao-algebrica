1663132d5514ecb385dd6f9014e2c232a66f2b92
Shikiyo <shikiyo@archjk>
2024-11-10 17:35:12 -0300

    trab: add basic tests and boolean domain

--- trab/singular.pl
+++ trab/singular.pl
@@ -1,12 +1,17 @@
 :- module(singular, [
   % Top level usage
-  compile//3, % compile//6,
+  compile//2, compile//7,
   % Output primitives
-  header//3,
-  % def_poly//2, def_polys//2, gendef_polys//3,
-  % def_ideal//2, print_ideal//1,
+  header//2,
+  def_poly_eq_dom_gen//5,
+  def_polys_eqs_dom_gen//6,
+  def_ideal//2, print_ideal//1,
   % Equation DSL
-  equation//2
+  equation//2,
+  % Domain primitives
+  domain_vars/2, domain_roots/2,
+  domain_vareqs/2, domain_vareqs/3,
+  domain_rooteqs/2, domain_rooteqs/3
 ]).
 
 :- use_module(library(clpz), [
@@ -14,71 +19,148 @@
   op(700, xfx, #=)
 ]).
 :- use_module(library(dcgs)).
+:- use_module(library(lambda)).
+:- use_module(library(lists), [foldl/4]).
+
+%%% Domain in Standard Prolog %%%
+
+:- meta_predicate(foldl_range(3, ?, ?, ?, ?)).
+foldl_range(Pred, Low, High, A0, A) :-
+  ( Low #< High,
+    Low1 #= Low + 1,
+    call(Pred, Low, A0, A1),
+    foldl_range(Pred, Low1, High, A1, A)
+  ; Low #= High,
+    A0 = A
+  ).
+
+domain_vars(unityroots(_, NVars), Vars) :-
+  foldl_range(
+    \Idx^[var(x, Idx) | Vs]^Vs^true,
+    0, NVars, Vars, []).
+domain_vars(boolean(BNs), Vars) :-
+  foldl(
+    \ (B-N)^Vs0^Vs^(
+      foldl_range(
+        \Idx^[var(B, Idx) | VVs]^VVs^true,
+        0, N, Vs0, Vs
+      )
+    ),
+    BNs, Vars, []
+  ).
+
+domain_roots(unityroots(N, _), Roots) :-
+  foldl_range(
+    \Idx^[var(w, Idx) | Vs]^Vs^true,
+    0, N, Roots, []
+  ).
+
+domain_vareqs(D, Eqs) :- domain_vareqs(D, Eqs, []).
+
+domain_vareqs(D, Eqs0, Eqs) :-
+  domain_vars(D, Vars),
+  foldl(
+    \X^[var_indomain(X) | Es]^Es^true,
+    Vars, Eqs0, Eqs
+  ).
+
+domain_rooteqs(D, Eqs) :- domain_rooteqs(D, Eqs, []).
+
+domain_rooteqs(unityroots(N, NVars), Eqs0, Eqs) :-
+  domain_roots(unityroots(N, NVars), Roots),
+  unityroots_rooteqs(Roots, Eqs0, X, X, Eqs).
+domain_rooteqs(boolean(_), Eqs, Eqs).
+
+unityroots_rooteqs([], DomEqs, DomEqs, DiffEqs, DiffEqs).
+unityroots_rooteqs([Root | Roots], DomEqs0, DomEqs, DiffEqs0, DiffEqs) :-
+  DomEqs0 = [var_indomain(Root) | DomEqs1],
+  unityroots_diffroots(Roots, Root, DiffEqs0, DiffEqs1),
+  unityroots_rooteqs(Roots, DomEqs1, DomEqs, DiffEqs1, DiffEqs).
+
+unityroots_diffroots([], _, DiffEqs, DiffEqs).
+unityroots_diffroots([RootB | Roots], RootA, DiffEqs0, DiffEqs) :-
+  DiffEqs0 = [var_diff(RootA, RootB) | DiffEqs1],
+  unityroots_diffroots(Roots, RootA, DiffEqs1, DiffEqs).
 
 %%% DCGs Hellpers %%%
 
 atom(A) --> { atom_chars(A, Cs) }, Cs.
 number(N) --> { number_chars(N, Cs) }, Cs.
 
+%%% BoolExpr %%%
+
+boolexpr(false) --> "0".
+boolexpr(true) --> "1".
+boolexpr(var(Name, Idx)) --> varname(var(Name, Idx)).
+boolexpr(not(A)) --> "1-(", boolexpr(A), ")".
+boolexpr(and(A, B)) --> "(", boolexpr(A), ")*(", boolexpr(B), ")".
+boolexpr(or(A, B)) -->
+  "(", boolexpr(A), ")+(", boolexpr(B), ")-((",
+  boolexpr(A), ")*(", boolexpr(B), "))".
+boolexpr(xor(A, B)) -->
+  "(", boolexpr(A), ")+(", boolexpr(B), ")-(2*(",
+  boolexpr(A), ")*(", boolexpr(B), "))".
+
 %%% Equation Helpers %%%
 
 varname(var(Name, Idx)) --> atom(Name), "(", number(Idx), ")".
 
-varname_exp(Var, Exp) --> varname(Var), "^", [Exp].
+varname_exp(Var, Exp) --> varname(Var), "^", number(Exp).
 
 varname_isname(Var, Is) --> varname(Var), " - ", varname(Is).
 
-domain_varname_in(unityroots(_), Var) --> varname(Var), " - 1".
+domain_varname_in(unityroots(N, _), Var) -->
+  varname_exp(Var, N), " - 1".
+domain_varname_in(boolean(_), Var) -->
+  varname(Var), "*(", varname(Var), " - 1)".
 
 domain_varname_diffname(D, Var, Diff) -->
   "((", domain_varname_in(D, Var), ") - (", domain_varname_in(D, Diff), "))/(",
   varname_isname(Var, Diff), ")".
 
-%% Std Prolog
+domain_boolexprs_count(unityroots(_,_), _, _) --> { false }.
+domain_boolexprs_count(boolean(_), [BExpr | BExprs], Count) -->
+  boolean_bexpr(BExpr),
+  boolean_bexprs_sum(BExprs, Count).
 
-domain_varidx_name(_, Idx, var(x, Idx)).
-domain_rootidx_name(_, Idx, var(w, Idx)).
+boolean_bexpr(BExpr) --> "(", boolexpr(BExpr), ")".
 
-domain_rooteqs(D, Eqs) :- domain_rooteqs(D, Eqs, []).
-
-domain_rooteqs(uniyroots(N), Eqs0, Eqs) :-
-  unityroots_rooteqs(0, N, Eqs0, X, X, Eqs).
-
-unityroots_rooteqs(Idx, N, DomEqs0, DomEqs, DiffEqs0, DiffEqs) :-
-  (
-    Idx #< N, Idx1 #= Idx + 1,
-    domain_rootidx_name(unityroots(N), Idx, Root),
-    DomEqs0 = [var_indomain(Root) | DomEqs1],
-    unityroots_rooteqs(Idx1, N, DomEqs1, DomEqs, DiffEqs1, DiffEqs)
-  ;
-    Idx #= N
-  ).
-
-% def_roots_dom_gen_unityroots(Idx, N, Gen0, Gen, PNames0, PNames) -->
-%   "poly ", gen_polyname(Gen0, Gen1, PName), " = ",
-%   ( { Idx #< N, Idx1 #= Idx + 1 },
-%   ).
-
-% def_roots_dom_gen(unityroots(N), Gen0, Gen, PNames0, PNames) --> def_roots_dom_gen_unityroots(0, N, Gen0, Gen, PNames0, PNames).
-%   TODO.
+boolean_bexprs_sum([], Sum) --> " - ", number(Sum).
+boolean_bexprs_sum([BExpr | BExprs], Sum) -->
+  " + ", boolean_bexpr(BExpr),
+  boolean_bexprs_sum(BExprs, Sum).
 
 %%% Equation DSL Interpreter %%%
 
 equation(var_indomain(Var), D) --> domain_varname_in(D, Var).
 equation(var_diff(Var, Diff), D) --> domain_varname_diffname(D, Var, Diff).
 equation(var_is(Var, Is), _) --> varname_isname(Var, Is).
+equation(boolexprs_count(BExprs, Count), D) --> domain_boolexprs_count(D, BExprs, Count).
 
 %%% Singular Helpers %%%
 
 endcmd --> ";\n".
 
-domain_defvars(unityroots(N), NVars) -->
-  { N1 #= N - 1, NVars1 #= NVars -  1 },
-  "x(0..", number(NVars1), "),w(0..", number(N1), ")".
+def_var_count(Name, N) -->
+  { N1 #= N - 1 },
+  atom(Name), "(0..", number(N1), ")".
+
+domain_defvars(unityroots(N, NVars)) -->
+  def_var_count(x, NVars), ",",
+  def_var_count(w, N).
+domain_defvars(boolean([B-N | Bs])) -->
+  def_var_count(B, N),
+  boolean_domain_defvars(Bs).
+
+boolean_domain_defvars([]) --> [].
+boolean_domain_defvars([B-N | Bs]) -->
+  ",", def_var_count(B, N),
+  boolean_domain_defvars(Bs).
 
 poly_name(prefixed(Prefix, PID)) --> atom(Prefix), number(PID).
 
-polys_names([PID | PIDs]) --> polys_names_(PIDs, PID).
+polys_names([PName | PNames]) -->
+  polys_names_(PNames, PName).
 
 polys_names_([], P) --> poly_name(P).
 polys_names_([PName | PNames], P) -->
@@ -89,22 +171,22 @@ ideal_name(name(Name)) --> atom(Name).
 
 %%% Singular Toplevel Commands %%%
 
-header(NVars, Dom, Ord) -->
+header(Dom, Ord) -->
   "option(redSB)", endcmd,
   "ring r = 0,(",
-  domain_defvars(Dom, NVars),
+  domain_defvars(Dom),
   "),", atom(Ord), endcmd.
 
 gen_polyname(gen_prefix(Prefix, PID), gen_prefix(Prefix, PID1), prefixed(Prefix, PID)) -->
   { PID1 #= PID + 1 }, poly_name(prefixed(Prefix, PID)).
 
-def_poly_eq_gen(Eq, Dom, PName, Gen0, Gen) -->
+def_poly_eq_dom_gen(Eq, Dom, PName, Gen0, Gen) -->
   "poly ", gen_polyname(Gen0, Gen, PName), " = ", equation(Eq, Dom), endcmd.
 
 def_polys_eqs_dom_gen([], _, Gen, Gen, PNames, PNames) --> [].
 def_polys_eqs_dom_gen([Eq | Eqs], Dom, Gen0, Gen, PNames0, PNames) -->
   { PNames0 = [PName | PNames1] },
-  def_poly_eq_gen(Eq, Dom, PName, Gen0, Gen1),
+  def_poly_eq_dom_gen(Eq, Dom, PName, Gen0, Gen1),
   def_polys_eqs_dom_gen(Eqs, Dom, Gen1, Gen, PNames1, PNames).
 
 def_ideal(IName, PNames) -->
@@ -112,12 +194,14 @@ def_ideal(IName, PNames) -->
 
 print_ideal(IName) --> ideal_name(IName), endcmd.
 
-compile(Eqs, NVars, Dom) -->
-  compile(Eqs, NVars, Dom, lp, gen_prefix(k, 0), gen_prefix(h, 0), name(i)).
-compile(Eqs, NVars, Dom, Ord, Root_gen, Poly_gen, IName) -->
-  header(NVars, Dom, Ord),
-  { domain_rooteqs(Dom, REqs) },
+compile(Eqs, Dom) -->
+  compile(Eqs, Dom, lp, gen_prefix(d, 0), gen_prefix(k, 0), gen_prefix(h, 0), name(i)).
+compile(Eqs, Dom, Ord, Root_gen, Dom_gen, Poly_gen, IName) -->
+  header(Dom, Ord),
+  { domain_rooteqs(Dom, REqs),
+    domain_vareqs(Dom, VEqs) },
   def_polys_eqs_dom_gen(REqs, Dom, Root_gen, _, PNames, PNames1),
-  def_polys_eqs_dom_gen(Eqs, Dom, Poly_gen, _, PNames1, []),
+  def_polys_eqs_dom_gen(VEqs, Dom, Dom_gen, _, PNames1, PNames2),
+  def_polys_eqs_dom_gen(Eqs, Dom, Poly_gen, _, PNames2, []),
   def_ideal(IName, PNames),
   print_ideal(IName).
--- trab/test.pl
+++ trab/test.pl
@@ -3,4 +3,38 @@
 
 :- use_module(singular).
 
-test(A) :- current_output(S), phrase_to_stream(A, S).
+test(A) :- current_output(S), test(A, S).
+
+test(A, S) :- phrase_to_stream(A, S).
+
+test_unityroots :- current_output(S), test_unityroots(S).
+test_unityroots(S) :-
+  D = unityroots(4, 2),
+  Eqs = [
+    var_diff(var(x, 0), var(x, 1)),
+    var_is(var(x, 1), var(w, 3))
+  ],
+  test(compile(Eqs, D), S), !,
+true.
+
+test_boolean :- current_output(S), test_boolean(S).
+test_boolean(S) :-
+  D = boolean([x-1, y-2, z-4]),
+  Eqs = [
+    var_diff(var(x, 0), var(y, 1)),
+    var_is(var(y, 1), var(z, 3)),
+    boolexprs_count([
+      false,
+      true,
+      var(z, 0),
+      var(z, 1),
+      var(z, 2),
+      var(z, 3),
+      not(var(x, 0)),
+      and(var(y, 0), var(y, 1)),
+      or(var(x, 0), var(y, 0)),
+      xor(var(z, 1), var(y, 1))
+    ], 3)
+  ],
+  test(compile(Eqs, D), S), !,
+true.
